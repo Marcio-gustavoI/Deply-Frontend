16fdbfef2e8281d697ca4179637633a2936bcb81
Marcio <Marcio.gustavvo@gmail.com>
2025-10-21 16:11:47 -0300

    atualização arquivos

---
+++ app.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
